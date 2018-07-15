ef69c85d9b711fea627e92a02a69c752989c134b
Hariprasad M <hari.prasad.m90@gmail.com>
2018-07-15 12:50:05 +0530

    1.Created model class to organize data 2. Created UI in activity_main.xml 3. Added toolbar code in MainActivity.java

--- app/src/main/java/com/hari/aboutplaceproject/MainActivity.java
+++ app/src/main/java/com/hari/aboutplaceproject/MainActivity.java
@@ -2,14 +2,19 @@ package com.hari.aboutplaceproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String DATA_URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json";
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        //Setting toolbar for Screen title
+        setSupportActionBar(mToolBar);
     }
 }
